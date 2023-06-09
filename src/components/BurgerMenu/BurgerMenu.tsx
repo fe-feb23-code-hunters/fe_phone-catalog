b0d7f38e5c01116307dd382ad07f6a33a51e1cbc
Hamuud <artemlask2014@gmail.com>
2023-06-09 16:08:27 +0300

    implement bem to the module scss classes

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -5,57 +5,72 @@ import HeartOutlined from '../../icons/HeartOutlined/HeartOutlined';
 import ShoppingBag from '../../icons/ShoppingBag/ShoppingBag';
 
 export const BurgerMenu = () => {
+  const {
+    burger,
+    container,
+    burger__content: content,
+    burger_top_actions: topActions,
+    logo__img: logoImg,
+    burger__top_close: closeLink,
+    burger__list: navList,
+    burger__item: navItem,
+    burger__link: navLink,
+    burger__bottom: bottomContainer,
+    burger__bottom_icon: bottomIcon,
+    burger__bottom_icon__left: bottomIconLeft,
+  } = classes;
+
   return (
-    <nav className={classes.burger} id="menu">
-      <div className={classes.container}>
-        <div className={classes.burger__content}>
-          <div className={classes.burger_top_actions}>
+    <nav className={burger} id="menu">
+      <div className={container}>
+        <div className={content}>
+          <div className={topActions}>
             <Link to="/" className="logo">
               <img
                 src={`${process.env.PUBLIC_URL}/img/logo/logo.png`}
                 alt="Nice Gadgets Logo"
-                className={classes.logo__img}
+                className={logoImg}
               />
             </Link>
-            <Link to="/" className={classes.burger__top_close}>
+            <Link to="/" className={closeLink}>
               <Close />
             </Link>
           </div>
 
-          <ul className={classes.burger__list}>
-            <li className={classes.burger__item}>
-              <Link to="/" className={classes.burger__link}>
+          <ul className={navList}>
+            <li className={navItem}>
+              <Link to="/" className={navLink}>
                 Home
               </Link>
             </li>
 
-            <li className={classes.burger__item}>
-              <Link to="/phone" className={classes.burger__link}>
+            <li className={navItem}>
+              <Link to="/phone" className={navLink}>
                 Phones
               </Link>
             </li>
 
-            <li className={classes.burger__item}>
-              <Link to="/tablets" className={classes.burger__link}>
+            <li className={navItem}>
+              <Link to="/tablets" className={navLink}>
                 Tablets
               </Link>
             </li>
 
-            <li className={classes.burger__item}>
-              <Link to="/accesories" className={classes.burger__link}>
+            <li className={navItem}>
+              <Link to="/accesories" className={navLink}>
                 Accesories
               </Link>
             </li>
           </ul>
 
-          <div className={classes.burger__bottom}>
-            <Link to="/favourite" className={classes.burger__bottom_icon}>
+          <div className={bottomContainer}>
+            <Link to="/favourite" className={bottomIcon}>
               <HeartOutlined />
             </Link>
 
             <Link
               to="/shoppingbag"
-              className={classes.burger__bottom_icon__left}
+              className={bottomIconLeft}
             >
               <ShoppingBag />
             </Link>
