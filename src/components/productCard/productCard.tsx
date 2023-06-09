66ccac14b8b42eefbd805828633e2fcd05fd47fa
Uladzislau Bichko <wladyslaw.biczko@gmail.com>
2023-06-09 16:35:17 +0200

    fix module name

--- src/components/productCard/productCard.tsx
+++ src/components/productCard/productCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './productCard.module.scss';
+import classes from './productCard.module.scss';
 
 type Props = {
   imgURL: string,
@@ -21,38 +21,38 @@ export const ProductCard: React.FC<Props> = ({
   title,
 }) => {
   return (
-    <div className={style.card}>
-      <div className={style.card__wrapper}>
+    <div className={classes.card}>
+      <div className={classes.card__wrapper}>
         <img
-          className={style.card__image}
+          className={classes.card__image}
           src={imgURL}
           alt="phone"
         />
-        <h2 className={style.card__title}>
+        <h2 className={classes.card__title}>
           {title}
         </h2>
-        <div className={style.card__price__wrapper}>
-          <h3 className={style.card__price}>{`$${price}`}</h3>
-          <h3 className={style.card__price__old}>{`$${oldPrice}`}</h3>
+        <div className={classes.card__price__wrapper}>
+          <h3 className={classes.card__price}>{`$${price}`}</h3>
+          <h3 className={classes.card__price__old}>{`$${oldPrice}`}</h3>
         </div>
-        <div className={style.card__specification__wrapper}>
-          <p className={style.card__specification__title}>Screen</p>
-          <p className={style.card__specification__value}>{screen}</p>
+        <div className={classes.card__specification__wrapper}>
+          <p className={classes.card__specification__title}>Screen</p>
+          <p className={classes.card__specification__value}>{screen}</p>
         </div>
-        <div className={style.card__specification__wrapper}>
-          <p className={style.card__specification__title}>Capacity</p>
-          <p className={style.card__specification__value}>{`${capacity}GB`}</p>
+        <div className={classes.card__specification__wrapper}>
+          <p className={classes.card__specification__title}>Capacity</p>
+          <p className={classes.card__specification__value}>{`${capacity}GB`}</p>
         </div>
-        <div className={style.card__specification__wrapper}>
-          <p className={style.card__specification__title}>RAM</p>
-          <p className={style.card__specification__value}>{`${ram}GB`}</p>
+        <div className={classes.card__specification__wrapper}>
+          <p className={classes.card__specification__title}>RAM</p>
+          <p className={classes.card__specification__value}>{`${ram}GB`}</p>
         </div>
-        <div className={style.card__buttons}>
-          <button className={style.card__buttons__cart} type="button">
+        <div className={classes.card__buttons}>
+          <button className={classes.card__buttons__cart} type="button">
             Add to cart
           </button>
           <div
-            className={style.card__buttons__favorite}
+            className={classes.card__buttons__favorite}
           />
         </div>
       </div>
