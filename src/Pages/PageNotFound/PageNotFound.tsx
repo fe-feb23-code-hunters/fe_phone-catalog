c491f002a8e5fe7a27a518824a24ffb6e3a9f911
Hamuud <artemlask2014@gmail.com>
2023-06-09 23:53:29 +0300

    Create Page not Found without animation

--- src/Pages/PageNotFound/PageNotFound.tsx
+++ src/Pages/PageNotFound/PageNotFound.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import classes from './PageNotFound.module.scss';
+
 export const PageNotFound: React.FC = () => (
-  <div>
-    <h1>Page Not Found</h1>
+  <div className={classes.container}>
+    <img
+      src={`${process.env.PUBLIC_URL}/img/PageNotFound/pageNotFound.svg`}
+      alt="page not found"
+      className={classes.page_not_found}
+    />
+    <h1>Ooops, looks like a ghost!</h1>
+    <br />
+    <h4>
+      The page you are looking for can&apos;t be found. Go home by &nbsp;
+      <Link to="/">clicking here!</Link>
+    </h4>
   </div>
 );
