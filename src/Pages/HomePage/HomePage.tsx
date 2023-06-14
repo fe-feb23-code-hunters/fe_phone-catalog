3fd39caf1565fe90dcd515276729f5b1f84b58cc
Uladzislau Bichko <wladyslaw.biczko@gmail.com>
2023-06-14 18:45:33 +0200

    rebuild first slider

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,49 +1,10 @@
-import { useContext, useState } from 'react';
-// eslint-disable-next-line max-len
-import { ProductsContext } from '../../providers/ProductsProvider/ProductsProvider';
-import { CartContext } from '../../providers/CartProvider/CartProvider';
-import Dropdown from '../../components/shared/Dropdown';
-import { DropdownOption } from '../../types/common';
+import { BunnerSlider } from '../../components/sliders/bunnerSlider';
 import CategorySection from './CategorySection/CategorySection';
-import MainTitle from './MainTitle/MainTitle';
-
-const DUMMY_OPTIONS = [
-  { value: 'option1', label: 'Option 1' },
-  { value: 'option2', label: 'Option 2' },
-  { value: 'option3', label: 'Option 3' },
-];
 
 const HomePage: React.FC = () => {
-  const { products } = useContext(ProductsContext);
-  const { addToCart } = useContext(CartContext);
-
-  const [selectedOption, setSelectedOption] = useState<DropdownOption>(
-    DUMMY_OPTIONS[0],
-  );
-
-  const onDropdownChange = (newOption: DropdownOption) => {
-    setSelectedOption(newOption);
-  };
-
   return (
     <div>
-      <MainTitle />
-      {/* Products for review, delete this */}
-      {products.map((product: { id: string; name: string }) => (
-        <div key={product.id}>
-          {product.name}
-          <button type="button" onClick={() => addToCart(product.id)}>
-            Add to cart
-          </button>
-        </div>
-      ))}
-
-      <Dropdown
-        options={DUMMY_OPTIONS}
-        selectedOption={selectedOption}
-        onChange={onDropdownChange}
-        label="Description"
-      />
+      <BunnerSlider />
       <CategorySection />
     </div>
   );
