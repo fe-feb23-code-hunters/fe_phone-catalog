c3429db03ec3a3f87fedb27dd2d424ddd77cef11
Hamuud <artemlask2014@gmail.com>
2023-06-14 10:14:36 +0300

    Create another folder for main title

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,13 +1,11 @@
 import { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-import cn from 'classnames';
 // eslint-disable-next-line max-len
 import { ProductsContext } from '../../providers/ProductsProvider/ProductsProvider';
 import { CartContext } from '../../providers/CartProvider/CartProvider';
-import classes from './HomePage.module.scss';
 import Dropdown from '../../components/shared/Dropdown';
 import { DropdownOption } from '../../types/common';
-import Category from '../../components/Category/Category';
+import CategorySection from './CategorySection/CategorySection';
+import MainTitle from './MainTitle/MainTitle';
 
 const DUMMY_OPTIONS = [
   { value: 'option1', label: 'Option 1' },
@@ -27,46 +25,9 @@ const HomePage: React.FC = () => {
     setSelectedOption(newOption);
   };
 
-  const {
-    container,
-    grid,
-    link,
-    main__title: mainTitle,
-    grid__desktop: gridDesktop,
-    grid__tablet: gridTablet,
-    grid__mobile: gridMobile,
-    'grid__row-mobile': gridMobileRow,
-    grid__item: gridItem,
-    grid__item__mobile_1_4: gridMobileFullSize,
-    grid__item__tablet_1_12: gridTabletFullSize,
-    grid__item__desktop_1_24: gridDesktopFullSize,
-    grid__item__desktop_1_8: gridDesktopStart,
-    grid__item__tablet_1_4: gridTabletStart,
-    grid__item__desktop_9_16: gridDesktopMiddle,
-    grid__item__tablet_5_8: gridTabletMiddle,
-    grid__item__desktop_17_24: gridDesktopEnd,
-    grid__item__tablet_9_12: gridTabletEnd,
-    category__title: categoryTitle,
-    container__top: containerTop,
-  } = classes;
-
   return (
     <div>
-      <div className={container}>
-        <div className={cn(grid, gridMobile, gridTablet, gridDesktop)}>
-          <div
-            className={cn(
-              gridItem,
-              gridMobileFullSize,
-              gridTabletFullSize,
-              gridDesktopFullSize,
-            )}
-          >
-            <h1 className={mainTitle}>Welcome to Nice Gadgets store!</h1>
-          </div>
-        </div>
-      </div>
-
+      <MainTitle />
       {/* Products for review, delete this */}
       {products.map((product: { id: string; name: string }) => (
         <div key={product.id}>
@@ -83,88 +44,7 @@ const HomePage: React.FC = () => {
         onChange={onDropdownChange}
         label="Description"
       />
-      <div className={container}>
-        <div className={cn(grid, gridMobile, gridTablet, gridDesktop)}>
-          <div
-            className={cn(
-              gridItem,
-              gridMobileFullSize,
-              gridTabletFullSize,
-              gridDesktopFullSize,
-            )}
-          >
-            <h2 className={categoryTitle}>Shop by category</h2>
-          </div>
-        </div>
-        <div
-          className={
-            cn(
-              grid,
-              gridMobile,
-              gridMobileRow,
-              gridTablet,
-              gridDesktop,
-              containerTop,
-            )
-          }
-        >
-          <div
-            className={cn(
-              gridItem,
-              gridMobileFullSize,
-              gridTabletStart,
-              gridDesktopStart,
-            )}
-          >
-            <Link to="/phones" className={link}>
-              <Category
-                imgUrl="/img/banners/categories/Category_Phones.jpg"
-                altImg="Phones category"
-                subtitle="Phones"
-                text="95 models"
-                color="#6D6474"
-              />
-            </Link>
-          </div>
-          <div
-            className={cn(
-              gridItem,
-              gridMobileFullSize,
-              gridTabletMiddle,
-              gridDesktopMiddle,
-            )}
-          >
-            <Link to="/tablets" className={link}>
-              <Category
-                imgUrl="/img/banners/categories/Category_Tablets.jpg"
-                altImg="Tablets category"
-                subtitle="Tablets"
-                text="24 models"
-                color="#8D8D92"
-              />
-            </Link>
-          </div>
-
-          <div
-            className={cn(
-              gridItem,
-              gridMobileFullSize,
-              gridTabletEnd,
-              gridDesktopEnd,
-            )}
-          >
-            <Link to="/accessories" className={link}>
-              <Category
-                imgUrl="/img/banners/categories/Category_Accessories.jpg"
-                altImg="Accessories category"
-                subtitle="Accessories"
-                text="100 models"
-                color="#973D5F"
-              />
-            </Link>
-          </div>
-        </div>
-      </div>
+      <CategorySection />
     </div>
   );
 };
