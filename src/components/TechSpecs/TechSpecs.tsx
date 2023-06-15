03e3d31418c33a762abe9ee1ee0f5800f3293c2e
Yuliia Anisimova <anisimova.yuliia01@gmail.com>
2023-06-15 10:43:57 +0300

    Added about and tech specs sections, fixed footer

---
+++ src/components/TechSpecs/TechSpecs.tsx
@@ -0,0 +1,78 @@
+import { Product } from '../../types/product';
+import classes from './TechSpecs.module.scss';
+
+const {
+  container,
+  'tech-specs__title': techSpecsTitle,
+  'tech-specs__list': techSpecsList,
+  'tech-specs__item': techSpecsItem,
+  'tech-specs__item-title': techSpecsItemTitle,
+  'tech-specs__item-info': techSpecsItemInfo,
+} = classes;
+
+interface Props {
+  product: Product;
+}
+
+export const TechSpecs: React.FC<Props> = ({ product }) => {
+  const {
+    phone,
+    capacity,
+    screen,
+    ram,
+  } = product;
+
+  return (
+    <div className={container}>
+      {product && (
+        <div>
+          <h2 className={techSpecsTitle}>Tech specs</h2>
+          <ul className={techSpecsList}>
+            <li className={techSpecsItem}>
+              <span className={techSpecsItemTitle}>Screen</span>
+              <span className={techSpecsItemInfo}>{screen}</span>
+            </li>
+            <li className={techSpecsItem}>
+              <span className={techSpecsItemTitle}>Resolution</span>
+              <span className={techSpecsItemInfo}>{phone?.resolution}</span>
+            </li>
+            <li className={techSpecsItem}>
+              <span className={techSpecsItemTitle}>Processor</span>
+              <span className={techSpecsItemInfo}>{phone?.processor}</span>
+            </li>
+            <li className={techSpecsItem}>
+              <span className={techSpecsItemTitle}>RAM</span>
+              <span className={techSpecsItemInfo}>{ram}</span>
+            </li>
+            <li className={techSpecsItem}>
+              <span className={techSpecsItemTitle}>Built in memory</span>
+              <span className={techSpecsItemInfo}>{capacity}</span>
+            </li>
+            <li className={techSpecsItem}>
+              <span className={techSpecsItemTitle}>Camera</span>
+              <span className={techSpecsItemInfo}>{phone?.camera}</span>
+            </li>
+            <li className={techSpecsItem}>
+              <span className={techSpecsItemTitle}>Zoom</span>
+              <span className={techSpecsItemInfo}>{phone?.zoom}</span>
+            </li>
+            <li className={techSpecsItem}>
+              <span className={techSpecsItemTitle}>Cell</span>
+              <span>
+                {phone?.cell.map(item => (
+                  <span
+                    className={techSpecsItemInfo}
+                    key={item}
+                  >
+                    {`${item} `}
+                  </span>
+                ))}
+
+              </span>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
