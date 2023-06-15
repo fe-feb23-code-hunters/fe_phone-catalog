e41ff470e41913b6d5d39f1c8b408705eaf969cb
Uladzislau Bichko <wladyslaw.biczko@gmail.com>
2023-06-15 11:36:53 +0200

    second slider

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,10 +1,14 @@
 import { BunnerSlider } from '../../components/sliders/bunnerSlider';
+import {
+  ProductSlider,
+} from '../../components/sliders/productSlider/productSlider';
 import CategorySection from './CategorySection/CategorySection';
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <BunnerSlider />
+      <ProductSlider />
       <CategorySection />
     </div>
   );
