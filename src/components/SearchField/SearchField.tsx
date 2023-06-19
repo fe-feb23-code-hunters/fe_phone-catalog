12734a6ff99d28d2105cf5eeacdc46a13cff44c4
Daria <hamdsad8@gmail.com>
2023-06-19 11:19:04 +0300

    fixed search bug

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,9 +1,4 @@
-import React, {
-  useState,
-  useEffect,
-  ChangeEventHandler,
-} from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect, ChangeEventHandler } from 'react';
 import Search from '../../icons/Search';
 import Close from '../../icons/Close';
 import classes from './SearchField.module.scss';
@@ -27,10 +22,6 @@ export const SearchField: React.FC<Props> = ({
 }) => {
   const [showClearButton, setShowClearButton] = useState(false);
 
-  const location = useLocation();
-  const isSearchVisible = location.pathname === '/phones'
-    || location.pathname === '/favourites';
-
   const handleClearInput = () => {
     handleClear();
     setShowClearButton(false);
@@ -41,34 +32,27 @@ export const SearchField: React.FC<Props> = ({
   }, [query]);
 
   return (
-    <>
-      {isSearchVisible && (
-        <form className={searchForm}>
-          <input
-            type="text"
-            className={searchFormInput}
-            value={query}
-            onChange={handleInputChange}
-            placeholder="Type something..."
-          />
-          {showClearButton ? (
-            <button
-              type="button"
-              className={searchFormButton}
-              onClick={handleClearInput}
-            >
-              <Close className="clear-icon" />
-            </button>
-          ) : (
-            <button
-              type="button"
-              className={searchFormButton}
-            >
-              <Search className="search-icon" />
-            </button>
-          )}
-        </form>
+    <form className={searchForm}>
+      <input
+        type="text"
+        className={searchFormInput}
+        value={query}
+        onChange={handleInputChange}
+        placeholder="Type something..."
+      />
+      {showClearButton ? (
+        <button
+          type="button"
+          className={searchFormButton}
+          onClick={handleClearInput}
+        >
+          <Close className="clear-icon" />
+        </button>
+      ) : (
+        <button type="button" className={searchFormButton}>
+          <Search className="search-icon" />
+        </button>
       )}
-    </>
+    </form>
   );
 };
