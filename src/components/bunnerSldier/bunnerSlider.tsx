85cd44017fb4a0f8e93686a0c3a10edb5dbf1084
Uladzislau Bichko <wladyslaw.biczko@gmail.com>
2023-06-19 12:19:24 +0200

    fix

--- src/components/bunnerSldier/bunnerSlider.tsx
+++ src/components/bunnerSldier/bunnerSlider.tsx
@@ -1,27 +1,51 @@
-import { Navigation, Autoplay, Pagination } from 'swiper';
+import { Navigation, Autoplay, Pagination, A11y } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  MdOutlineKeyboardArrowLeft,
+  MdOutlineKeyboardArrowRight,
+} from 'react-icons/md';
 import bunner1 from '../../banners/carousel/Carousel_Front.jpg';
+import bunner2 from '../../banners/carousel/Carousel_Second.jpg';
+import bunner3 from '../../banners/carousel/Carousel_Third.jpeg';
 
 import 'swiper/css/bundle';
 import './bunnerSlider.scss';
 
 export const BunnerSlider = () => {
   return (
-    <Swiper
-      className="bunner__slider"
-      modules={[Navigation, Autoplay, Pagination]}
-      spaceBetween={16}
-      slidesPerView={1}
-      autoplay
-      navigation
-      pagination
-    >
-      <SwiperSlide>
-        <img className="bunner__slider-img" src={bunner1} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className="bunner__slider-img" src={bunner1} alt="" />
-      </SwiperSlide>
-    </Swiper>
+    <div className="bunner__slider">
+      <Swiper
+        modules={[Navigation, Autoplay, Pagination, A11y]}
+        slidesPerView={1}
+        autoplay
+        loop
+        navigation={{
+          nextEl: '.arrow-right',
+          prevEl: '.arrow-left',
+        }}
+        pagination={{
+          el: '.custom-pagination',
+          clickable: true,
+        }}
+      >
+        <SwiperSlide>
+          <img className="bunner__slider-img" src={bunner1} alt="bunner" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="bunner__slider-img" src={bunner2} alt="bunner" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="bunner__slider-img" src={bunner3} alt="bunner" />
+        </SwiperSlide>
+      </Swiper>
+
+      <div className="arrow-left">
+        <MdOutlineKeyboardArrowLeft />
+      </div>
+      <div className="arrow-right">
+        <MdOutlineKeyboardArrowRight />
+      </div>
+      <div className="custom-pagination" />
+    </div>
   );
 };
