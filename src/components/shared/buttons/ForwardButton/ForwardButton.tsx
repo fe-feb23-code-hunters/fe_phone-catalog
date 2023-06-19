cf79008bd198159f1f53971750732d75f55c297e
Anna Latusha <annapop03072701@gmail.com>
2023-06-19 14:54:57 +0300

    Disabled navigation button

--- src/components/shared/buttons/ForwardButton/ForwardButton.tsx
+++ src/components/shared/buttons/ForwardButton/ForwardButton.tsx
@@ -1,13 +1,21 @@
+import cn from 'classnames';
 import Arrow from '../../../../icons/Arrow/Arrow';
 import classes from './ForwardButton.module.scss';
 
 interface Props {
   label?: string;
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 const ForwardButton: React.FC<Props> = ({ label = 'Forward', onClick }) => (
-  <button type="button" onClick={onClick} className={classes.button}>
+  <button
+    type="button"
+    onClick={onClick}
+    className={cn(classes.button, {
+      [classes.disabled]: !onClick,
+    })}
+    disabled={!onClick}
+  >
     <Arrow className={classes.icon} />
     {label}
   </button>
